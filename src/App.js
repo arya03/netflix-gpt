e96d7b180701e22c3f171ca2307a7d26dff7e810
aryachauhan <106148200+aryachauhan@users.noreply.github.com>
2024-02-18 00:18:52 +0530

    cra and initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='text-lg font-bold text-green-600 text-center'>
+          Hello, we are ready to build our project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
